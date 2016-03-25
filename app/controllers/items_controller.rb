c122a2e2e10c0b5dac2114c9ccdb609c018150c7
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-03-25 00:47:10 -0500

    will have to fix destroy.js later

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
 
-  def new
-    @item = Item.new
-  end
-  
-  def create
-    @list = List.find(params[:list_id])
-    @item = @list.items.build(item_params)
+def new
+  @item = Item.new
+end
+
+def create
+  @list = List.find(params[:list_id])
+  @item = @list.items.build(item_params)
     if @item.save
       respond_to do |format|
         format.html { redirect_to list_path(@list) }
@@ -15,27 +15,28 @@ class ItemsController < ApplicationController
   end
 end
 
-  def index
-  end
+def index
+end
 
-  def edit
-  end
+def edit
+end
 
-  def destroy
-    @list = List.find(params[:list_id])
-    @item = @list.items.find(params[:id])
-    @item.destroy
-    redirect_to list_path(@list)
-  end
+def destroy
+  @list = List.find(params[:list_id])
+  @item = @list.items.find(params[:id])
+    if @item.destroy
+      redirect_to list_path(@list)
+    end
+end
 
-  def show
-  end
+def show
+end
 
-  private
+private
 
-  def item_params
-    params.require(:item).permit(:content)
-  end
+def item_params
+  params.require(:item).permit(:content)
+end
 
 end
 
